f8114feec94212f9f6dbf308000921d7001d5ce2
Helgard Richard Ferreira <hrfer3@gmail.com>
2021-02-07 16:04:24 +0200

    Updates

---
+++ precache-manifest.d8b6f3055744f26d775a07b85a9d038f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafdcf63e825045b68861ad67fab0874",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83f18108e11b7f34f15a",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "37f6c1d7c5a50b5a7e78",
+    "url": "./static/js/2.ec9bac39.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ec9bac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f18108e11b7f34f15a",
+    "url": "./static/js/main.cf832a50.chunk.js"
+  },
+  {
+    "revision": "0963e2fb1e7974323006",
+    "url": "./static/js/runtime-main.83a55aa8.js"
+  }
+]);
